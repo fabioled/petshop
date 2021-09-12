bbcbf99b455c3b7505976fed428c9fbbbea67131
Fábio Cordeiro <fabioled@gmail.com>
2021-09-12 16:37:01 -0300

    Projeto Criado

---
+++ src/test/java/com/fabio/petshop/PetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabio.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
