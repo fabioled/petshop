65108fcd5e9f5aebe1227b0215d96a84f7d859cf
Fábio Cordeiro <fabioled@gmail.com>
2021-09-12 22:22:05 -0300

    Testar Fachada Categorias

---
+++ src/main/java/com/fabio/petshop/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.fabio.petshop.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String mostrar() {
+		return "Teste REST Categorias!";
+	}
+
+}
