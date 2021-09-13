ce575c61666a39cd2aea011417ec6e91450bb84f
Fábio Cordeiro <fabioled@gmail.com>
2021-09-13 11:38:28 -0300

    Adicionado classe de Modelo Categoria e Rest

--- src/main/java/com/fabio/petshop/resources/CategoriaResource.java
+++ src/main/java/com/fabio/petshop/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.fabio.petshop.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fabio.petshop.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String mostrar() {
-		return "Teste REST Categorias!";
+	public List<Categoria> mostrar() {
+		
+		Categoria cat1 = new Categoria(1, "Alimento");
+		Categoria cat2 = new Categoria(2, "Cosmético");
+		Categoria cat3 = new Categoria(3, "Remédio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		lista.add(cat3);
+		
+		return lista;
 	}
 
 }
