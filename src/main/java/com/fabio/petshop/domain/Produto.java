bd1aa8b38d44aaab0ebeb6f2bd8d96fec85250e8
Fábio Cordeiro <fabioled@gmail.com>
2021-09-14 18:26:02 -0300

    Popula dados Produto e Categoria, associação muitos para muitos

--- src/main/java/com/fabio/petshop/domain/Produto.java
+++ src/main/java/com/fabio/petshop/domain/Produto.java
@@ -8,10 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable{
+public class Produto implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -19,19 +21,24 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+				joinColumns = @JoinColumn(name = "id_produto"),
+				inverseJoinColumns = @JoinColumn(name = "id_categoria"))	
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	public Categoria() {
+	public Produto() {
 		
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
-	}		
+		this.preco = preco;
+	}	
 
 	@Override
 	public int hashCode() {
@@ -49,7 +56,7 @@ public class Categoria implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -74,12 +81,23 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public Double getPreco() {
+		return preco;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
-	}	
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
 	
+	
+	
+		
 }
--- src/main/java/com/fabio/petshop/utils/PopulaDados.java
+++ src/main/java/com/fabio/petshop/utils/PopulaDados.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.fabio.petshop.domain.Categoria;
+import com.fabio.petshop.domain.Produto;
 import com.fabio.petshop.repository.CategoriaRepository;
+import com.fabio.petshop.repository.ProdutoRepository;
 
 @Component
 public class PopulaDados {
@@ -16,14 +18,32 @@ public class PopulaDados {
 	@Autowired
 	CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
 	@PostConstruct
 	public void cadastrar() {
 		
 		Categoria cat1 = new Categoria(null, "Alimento");
-		Categoria cat2 = new Categoria(null, "Cosmético");
-		Categoria cat3 = new Categoria(null, "Remédio");
+		Categoria cat2 = new Categoria(null, "Remédio");
+		Categoria cat3 = new Categoria(null, "Cosmético");
+			
+		Produto p1 = new Produto(null, "Ração", 100.0);
+		Produto p2 = new Produto(null, "Sache", 80.0);
+		Produto p3 = new Produto(null, "Shampoo", 50.0);
+		Produto p4 = new Produto(null, "Vermifugo", 20.0);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2));
+		cat2.getProdutos().addAll(Arrays.asList(p3, p4));
+		cat3.getProdutos().addAll(Arrays.asList(p4));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat2));
+		p4.getCategorias().addAll(Arrays.asList(cat2,cat3));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3,p4));
 		
 	}
 
